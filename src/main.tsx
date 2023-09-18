9b6e0fea6a49c6bcf8f2c73e0d0689aa827ad0b5
pot-code <136290354@qq.com>
2023-09-18 23:12:07 +0800

    chore: upgrade mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-import { MantineProvider } from "@mantine/core"
 import React from "react"
 import { createRoot } from "react-dom/client"
-
+import { createTheme, MantineProvider } from "@mantine/core"
 import App from "./app"
 import setup from "./setup"
-
 import "./styles/main.scss"
 
 const root = createRoot(document.getElementById("root") as Element)
+const theme = createTheme({})
 
 setup().then(() =>
   root.render(
     <React.StrictMode>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MantineProvider theme={theme}>
         <App />
       </MantineProvider>
     </React.StrictMode>,
